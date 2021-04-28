36e8eea4ed4fcdb3f5a9eaab8ca03e9ff3e4756d
Muhammad B Khan <>
2021-04-28 10:35:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#button1").click(function() {
+    $("#t1").hide();
+  });
+    $("#button2").click(function() {
+    $("#t2").hide();
+  });
+    $(".button3").click(function() {
+    $(".t3").hide();
+  });
+});
